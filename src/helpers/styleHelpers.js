b08debba23c05dee81dc89c9103563f9c7cd59a8
mehmet-karahalil <mehmetemin.karahaliloglu@gmail.com>
2023-08-19 22:34:54 +0300

    visual update

--- src/helpers/styleHelpers.js
+++ src/helpers/styleHelpers.js
@@ -1,21 +1,25 @@
 export const fixedMenuStyle = {
-  margin:0,
-  border:"none",
-  outline:"none",
+  margin: 0,
+  border: "none",
+  outline: "none",
   backgroundColor: "#fff",
-  color:"red",
+  color: "red",
 };
 
 export const fixedDarkMenuStyle = {
-  margin:0,
-  border:"none",
-  outline:"none",
+  margin: 0,
+  border: "none",
+  outline: "none",
   backgroundColor: "black",
-  color: "white"
+  color: "white",
 };
 
-export const pagecss={
-  position:"relative",
-  width:"1700px",
-  margin : "0 auto" 
-}
+export const pagecss = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100%",
+  width: "95%",
+  margin: "0 auto",
+};
